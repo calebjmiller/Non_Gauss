33cfd2651329311c9dccbebfae97194a63058db4
calebjmiller <cami1103@colorado.edu>
2018-07-17 16:12:11 -0600

    batch for thinned super darn data

---
+++ sd_thin_batch.m
@@ -0,0 +1,31 @@
+%%%% Batch file for real hi-lat data
+%%%% Repeating components removed
+
+% random seed for consistency
+eta_seed = 2;
+% reps
+R=1;
+% parameters
+tau = 0.1;
+B = 2;
+j_min = 2;
+j_max = 3;
+alpha = 3;
+
+sigma_j = B.^(-alpha/2*(j_min:j_max))*10;
+% seeding random numbers for consistency
+rng(eta_seed)
+
+knots = [0 0 0 0 0.5 1 1 1 1]*pi;
+%%%% where does this r come from
+r = 4;
+
+
+[post_samples,Y , theta, phi, Npix] = sd_thin_sim(R, eta_seed);
+
+%%%saving the reults
+filename = ['sd_thin_test_1_', num2str(eta_seed), '.mat'];
+save(filename)
+
+
+exit
